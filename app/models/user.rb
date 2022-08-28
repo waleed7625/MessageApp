21848382fd2a890ecafded1d38279aa3eda36beb
waleed7625 <waleedbadar0007@gmail.com>
2022-08-29 02:08:56 +0500

    added message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minimum: 3, maximum: 15 }
-    validates_uniqueness_of :username
+    validates :username, presence: true, length: { minimum: 3, maximum: 15 },
+        uniqueness: { case_sensitive: false }
     has_secure_password
+    has_many :messages
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_28_195341) do
+ActiveRecord::Schema.define(version: 2022_08_28_210513) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "username"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2022_08_28_195341) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "users"
 end
