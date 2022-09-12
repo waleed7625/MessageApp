dc5b0ffdf5c13fb9c566ea91bb49e806d499ff08
waleed7625 <waleedbadar0007@gmail.com>
2022-09-12 23:12:21 +0500

    add authentication system create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+    flash[:success] = "you have sucessfully logged in"
+    redirect_to  root_path
+    elsif user && !user.authenticate(params[:session][:password])
+    flash.now[:error] = "please enter your password"
+    render 'new'
+    else  
+      flash.now[:error] = "your username or password is not correct please try again"
+      render 'new'
+    end
+  end
+
+  def destroy
+   session[:user_id] = nil
+  flash[:success] = 'you have sucessfully logged out'
+  redirect_to login_path  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'login',to:'sessions#new'
- root 'chat_room#index'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  root 'chat_room#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
