ce1ff22d77700129c66a12d2689bd7d42f4905e5
waleed7625 <waleedbadar0007@gmail.com>
2023-03-14 18:16:52 +0500

    238-adding auto-scrolling to chat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,17 @@
 //= require semantic-ui
 //= require_tree .
 
-$(document).on('turbolinks:load', function(){
-$('.ui.dropdown').dropdown();
+scroll_bottom = function () {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+};
 
-$('.message .close').on('click', function() {
-    $(this).closest('.message').transition('fade');
-  })
-})
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
+  scroll_bottom();
+});
