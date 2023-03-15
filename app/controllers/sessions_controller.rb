7636d9202c602234aa9db858e4dbd698e1dca0c0
waleed7625 <waleedbadar0007@gmail.com>
2023-03-16 00:08:16 +0500

    integrate signup functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,29 @@
 class SessionsController < ApplicationController
   before_action :logged_in_redirect, only: %i(new create)
-  def new
+  before_action :user_params, only: %i(user_create)
+  
+  def user_new
+    @user = User.new
   end
+
+  def user_create
+    @user = User.new(user_params)
+    pass = @user.password
+    pass_digest = BCrypt::Password.create(pass)
+    @user.update(password_digest: pass_digest)
+    if @user.valid?
+      if @user.save!
+        session[:user_id] = @user.id
+        flash[:success] = "you have sucessfully sign up and logged"
+        redirect_to root_path
+      end
+    else 
+      flash[:error] = @user.errors.full_messages[0]
+      render "user_new"
+    end
+  end
+
+  def new; end
 
   def create
     user = User.find_by(username: params[:session][:username])
@@ -27,6 +49,10 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:username, :password_digest)
+  end
 
   def logged_in_redirect
     if logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'chat_room#index'
   get 'login',to:'sessions#new'
   post 'login', to: 'sessions#create'
+  get 'signup',to:'sessions#user_new'
+  post 'signup', to: 'sessions#user_create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
   
